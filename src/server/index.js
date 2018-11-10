d306b1ca452c4ed26ac51662811a69fd30cba7bb
Patrick Slagle <slaglebagel17@gmail.com>
2018-11-09 20:04:27 -0800

    set up server with routes

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const schema = require('./graphql-server');
+const schema = require('./graphql/schema.js');
+const bodyParser = require('body-parser');
 const app = express();
-const cors = require('cors'); 
 
-// allow cross-origin requests while in development mode and while running the front and back
-// end on the same computer. Without cors, the front end can't make requests to the server
-app.use(cors()) 
 
 const mongoose = require('mongoose');
 const Mongo_URI = process.env.MONGO_URI;
@@ -15,10 +12,16 @@ const Mongo_URI = process.env.MONGO_URI;
 mongoose.connect(Mongo_URI, { useNewUrlParser: true });
 mongoose.connection.once('open', () => console.log('connected to database'))
 
-app.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql: false //Set to true to view GraphiQl in browser at /graphql
-}));
+app.use(bodyParser.json())
+
+const GraphQLServer = graphqlHTTP({
+  schema,
+  graphiql: true //Set to true to view GraphiQl in browser at /graphql
+})
+
+app.get('/reservation/id', GraphQLServer);
+app.get('/reservations', GraphQLServer);
+app.post('/reservation', GraphQLServer);
 
 app.listen(4000, () => {
   console.log('Listening on 4000')
