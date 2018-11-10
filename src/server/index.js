bba72010907d6483d3021038d29dc57100b2fdca
Patrick Slagle <slaglebagel17@gmail.com>
2018-11-10 15:15:21 -0800

    modified server

--- src/server/index.js
+++ src/server/index.js
@@ -5,7 +5,6 @@ const schema = require('./graphql/schema.js');
 const bodyParser = require('body-parser');
 const app = express();
 
-
 const mongoose = require('mongoose');
 const Mongo_URI = process.env.MONGO_URI;
 
@@ -19,10 +18,20 @@ const GraphQLServer = graphqlHTTP({
   graphiql: true //Set to true to view GraphiQl in browser at /graphql
 })
 
-app.get('/reservation/id', GraphQLServer);
+app.get('/reservation/id', (req, res, next) => {
+  console.log(res.data)
+  console.log(req.data)
+  console.log(req.query)
+  console.log(res.query)
+  next()
+},GraphQLServer);
 app.get('/reservations', GraphQLServer);
 app.post('/reservation', GraphQLServer);
-app.use('/graphql', GraphQLServer)
+app.use('/graphql', (req, res, next) => {
+  console.log(req.query);
+  // console.log(req.body)
+  next(); 
+},GraphQLServer)
 
 app.listen(4000, () => {
   console.log('Listening on 4000')
