2f7af77f96c011e6bcce766fec1ee66a21ab8e98
Patrick Slagle <slaglebagel17@gmail.com>
2018-11-11 16:32:22 -0800

    deconstructed component to be made of several pure components

--- src/client/navigation/MakeReservation.js
+++ src/client/navigation/MakeReservation.js
@@ -1,168 +1,129 @@
 import React from 'react'
-import {
-  View,
-  StyleSheet,
-  TextInput,
-  Button
-} from 'react-native'
+import { View, Alert } from 'react-native'
 import { addReservationMutation } from '../graphql/mutations.js'
-import Calendar from '../components/Calender.js'
+import Calendar from '../components/Calendar.js'
+import TxtInput from '../components/TextInput.js'
+import Button from '../components/Button.js'
 class MakeReservation extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       name: '',
       hotelName: '',
-      dates: {},
-      arrivalDate: '',
-      departureDate: '',
-      calendar: false
+      arrivalDate: null,
+      departureDate: null,
+      showCalendar: false
     }
-  }
-
-  onChangeText = (key, value) => {
-    console.log(key);
-    console.log(value); 
-    this.setState({ [key]: value })
-  }
-
-  submitReservation = () => {
-    console.log(this.state)
-    const url = 'http://192.168.1.78:4000/reservation'
-    const data = {
-      query: addReservationMutation,
-      variables: {
-        name: this.state.name,
-        hotelName: this.state.hotelName,
-        arrivalDate: `${new Date(this.state.arrivalDate).toLocaleDateString()}`,
-        departureDate: `${new Date(this.state.departureDate).toLocaleDateString()}`
-      }
-    }
-    fetch(url, {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then(res => res.json())
-    .then(response => console.log('yasss', JSON.stringify(response)))
-    .catch(error => console.log('error', error))
   }
 
   /**
-   * @description for every date pressed, dayPress will do one of the following:
-   * if there is no arrival date or departure date, it will set an arrival date
-   * if there is an arrival date but no departure date, it will set a departure date.
-   * if there is an arrival and departure date, it reset the previous set dates, and set just an arrival date.
-   * @param {object} dayinfo - contains date info on the date pressed
+   * @description for every input change, update state
+   * @param {String} key - Identifies what key to update in state
+   * @param {String} value - The value of the input field
    */
-  dayPress = (dayInfo) => {
-    console.log('DAY PRESS', dayInfo)
-    // Either no days selected, or both days selected. Either way, only show the recently selected date.
-    if (!Object.keys(this.state.dates).length || Object.keys(this.state.dates).length > 1) {
-      this.setState({
-        arrivalDate: dayInfo.timestamp,
-        departureDate: '',
-        dates: {
-          [dayInfo.dateString]: {
-            textColor: 'white', 
-            startingDay: true,
-            endingDay: true, 
-            color: 'dodgerblue'
-          }
-        }
-      })
-    }
-    // arrival day selected, but departure date has not been set yet. Set departure date.
-    else {
-      const dates = {};
-      // iterate one day at a time from arrival date to departure date, making a date for each
-      // day to mark the range on the calendar. There are 86400000 milliseconds in a day
-      for (let i = this.state.arrivalDate; i <= dayInfo.timestamp; i += 86400000) {
-        const date = new Date(i).toISOString().slice(0, 10)
-        dates[date] = {
-          textColor: 'white', 
-          startingDay: i === this.state.arrivalDate ? true : false,
-          endingDay: i === dayInfo.timestamp ? true : false, 
-          color: 'dodgerblue'
-        }
-      }
-      this.setState({
-        departureDate: dayInfo.timestamp,
-        dates
-      })
-    }
+  onChangeText = (key, value) => {
+    this.setState({ [key]: value })
   }
 
-  renderCalendar(){
-    if (!this.state.calendar) return null
-
-    return (
-      <Calendar
-        dayPress={this.dayPress}
-        dates={this.state.dates}
-      />
-    )
-  }
-
-  toggleCalender(){
+  /**
+   * @description user submitted their dates from Calendar component. Update MakeReservation's
+   * arrivalDate and departureDate while also hiding the calendar
+   * @param {Number} arrivalDate - the arrival date in milliseconds since January 1, 1970
+   * @param {Number} departureDate - the departure date in milliseconds since January 1, 1970
+   */
+  getDates = (arrivalDate, departureDate) => {
     this.setState({
-      calendar: !this.state.calendar
+      arrivalDate, 
+      departureDate,
+      showCalendar: false
     })
   }
-  
+
+  /**
+   * @description sends a GraphQL mutation to the server to add a reservation with the
+   * following information from state: name, hotelName, arrivalDate, departureDate. 
+   */
+  submitReservation = () => {
+    // Alert user to fill out form if a portion is incomplete
+    if (!this.state.name.length || !this.state.hotelName.length || !this.state.arrivalDate || !this.state.departureDate) {
+      Alert.alert(
+        'Incomplete Reservation',
+        'Please input your name, the hotel name, and your dates requested',
+        [{text: 'OK'}],
+      )
+    } 
+    // if form is complete, send to server to add
+    else {
+      const IPAddress = `192.168.1.78`
+      const url = `http://${IPAddress}:4000/reservation`
+      const data = {
+        query: addReservationMutation,
+        variables: {
+          name: this.state.name,
+          hotelName: this.state.hotelName,
+          arrivalDate: `${new Date(this.state.arrivalDate).toLocaleDateString()}`,
+          departureDate: `${new Date(this.state.departureDate).toLocaleDateString()}`
+        }
+      }
+      fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(res => res.json())
+      .then(response => {
+        console.log('yasss', JSON.stringify(response))
+        this.setState({
+          name: '',
+          hotelName: '',
+          arrivalDate: null,
+          departureDate: null,
+          showCalendar: false
+        })
+      })
+      .catch(error => console.log('error', error))
+    }
+  }
+
+  /**
+   * @description toggles the calendar to show or not
+   */
+  toggleCalendar = () => {
+    this.setState({
+      showCalendar: !this.state.showCalendar
+    });
+  }
   
   render() {
-    console.log(this.state.arrivalDate)
-    console.log('IN COMP', this.state.dates)
     return (
-      <View style={styles.container}>
-        <TextInput
-          style={styles.input}
-          onFocus={this.randomFunc}
-          placeholder='Name'
-          autoCapitalize="words"
-          autoCorrect={false}
-          placeholderTextColor='black'
-          onChangeText={customerName => this.onChangeText('name', customerName)}
+      <View style={{flex: 1}}>
+        <Calendar
+          sendDates={this.getDates}
+          showCalendar={this.state.showCalendar}
+          toggleCalendar={this.toggleCalendar}
         />
-        <TextInput
-          style={styles.input}
-          placeholder='Hotel Name'
-          autoCapitalize="words"
-          placeholderTextColor='black'
-          onChangeText={hotelName => this.onChangeText('hotelName', hotelName)}
+        <TxtInput
+          onChangeText={this.onChangeText}
+          inputName='name'
+          placeholderText='Name'
+        />
+        <TxtInput
+          onChangeText={this.onChangeText}
+          inputName='hotelName'
+          placeholderText='Hotel Name'
         />
         <Button
-          onPress={() => this.toggleCalender()}
-          title="Dates"
+          onPress={this.toggleCalendar}
+          buttonText='Select Dates'
         />
-        {this.renderCalendar()}
         <Button
-          title='Submit'
           onPress={this.submitReservation}
+          buttonText='Submit Reservation'
         />
       </View>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  input: {
-    width: 350,
-    fontSize: 18,
-    fontWeight: '500',
-    height: 55,
-    backgroundColor: 'white',
-    margin: 10,
-    color: 'white',
-    padding: 8,
-    borderRadius: 14
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-  }
-})
 
 export default MakeReservation;
